5d372045cf3c2b74f2c3c899e381d3fa8c2c277d
Björn Harrtell <bjorn@wololo.org>
2023-03-31 12:34:06 +0200

    Avoid double token read on junk after last token

--- Utf8JsonStreamReader/Utf8JsonStreamReader.cs
+++ Utf8JsonStreamReader/Utf8JsonStreamReader.cs
@@ -24,51 +24,79 @@ namespace Wololo.Text.Json
             pipeReader = PipeReader.Create(stream, new StreamPipeReaderOptions(null, this.bufferSize, this.bufferSize, leaveOpen));
         }
 
+        private bool Finish()
+        {
+            TokenType = JsonTokenType.None;
+            Value = null;
+            done = true;
+            return true;
+        }
+
+        private async Task ReadAtLeastAsync(CancellationToken cancellationToken)
+        {
+            var readResult = await pipeReader.ReadAtLeastAsync(this.bufferSize, cancellationToken);
+            buffer = readResult.Buffer;
+            bytesConsumed = 0;
+            endOfStream = readResult.IsCompleted;
+        }
+
+        private bool FinishEarly()
+        {
+            // if end of stream and all is consumed already we are done
+            // this can happen if there is junk data in the stream after the last token
+            if (endOfStream && bytesConsumed == buffer.Length)
+                return Finish();
+            return false;
+        }
+
+        private void Advance()
+        {
+            if (bytesConsumed > 0)
+                pipeReader.AdvanceTo(buffer.GetPosition(bytesConsumed));
+        }
+
+        private void TryRead() {
+            if (buffer.Length > 0 && !Read(endOfStream))
+                throw new Exception("Invalid JSON or token too large for buffer");
+        }
+
+        private void DetermineDone() {
+            // if we are at end of stream and all is consumed we are done
+            if (endOfStream && bytesConsumed == buffer.Length)
+                done = true;
+        }
+
         public async Task<bool> ReadAsync(CancellationToken cancellationToken = default)
         {
             if (done)
-            {
-                TokenType = JsonTokenType.None;
-                Value = null;
-                return false;
-            }
+                return !Finish();
+            // if first read condition or if read fails
             if (TokenType == JsonTokenType.None || !Read(endOfStream))
             {
-                if (bytesConsumed > 0)
-                    pipeReader.AdvanceTo(buffer.GetPosition(bytesConsumed));
-                var readResult = await pipeReader.ReadAtLeastAsync(this.bufferSize, cancellationToken);
-                buffer = readResult.Buffer;
-                bytesConsumed = 0;
-                endOfStream = readResult.IsCompleted;
-                if ((buffer.Length - bytesConsumed) > 0 && !Read(endOfStream))
-                    throw new Exception("Invalid JSON or token too large for buffer");
+                if (FinishEarly())
+                    return false;
+                Advance();
+                await ReadAtLeastAsync(cancellationToken);
+                TryRead();
             }
-            if (endOfStream && bytesConsumed == buffer.Length)
-                done = true;
+            DetermineDone();
             return true;
         }
 
         public bool Read()
         {
             if (done)
-            {
-                TokenType = JsonTokenType.None;
-                Value = null;
-                return false;
-            }
+                return !Finish();
+            // if first read condition or if read fails
             if (TokenType == JsonTokenType.None || !Read(endOfStream))
             {
-                if (bytesConsumed > 0)
-                    pipeReader.AdvanceTo(buffer.GetPosition(bytesConsumed));
-                var readResult = pipeReader.ReadAtLeastAsync(this.bufferSize, CancellationToken.None).AsTask().GetAwaiter().GetResult();
-                buffer = readResult.Buffer;
-                bytesConsumed = 0;
-                endOfStream = readResult.IsCompleted;
-                if ((buffer.Length - bytesConsumed) > 0 && !Read(endOfStream))
-                    throw new Exception("Invalid JSON or token too large for buffer");
+                if (FinishEarly())
+                    return false;
+                Advance();
+                ReadAtLeastAsync(CancellationToken.None).GetAwaiter().GetResult();
+                TryRead();
             }
-            if (endOfStream && bytesConsumed == buffer.Length)
-                done = true;
+            DetermineDone();
             return true;
         }
 
