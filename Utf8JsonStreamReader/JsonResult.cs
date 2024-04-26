8ff12188cc59af520fccf2d342c1fafc8aec53db
Björn Harrtell <bjorn@septima.dk>
2024-04-26 23:15:37 +0200

    Unwrap JsonResult

--- Utf8JsonStreamReader/JsonResult.cs
+++ Utf8JsonStreamReader/JsonResult.cs
@@ -2,7 +2,4 @@ using System.Text.Json;
 
 namespace Wololo.Text.Json;
 
-public sealed partial class Utf8JsonStreamReader
-{
-    public readonly record struct JsonResult(JsonTokenType TokenType = JsonTokenType.None, object? Value = null);
-}
+public readonly record struct JsonResult(JsonTokenType TokenType = JsonTokenType.None, object? Value = null);
